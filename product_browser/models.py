664f3b30051b95abc7e0c11a286f9e47ed3dc06d
Carlos López <carlos@blewblew.com>
2010-08-10 22:14:49 -0500

    Se agregaron los modelos: Order, MenuCategory, MenuItem, OrderItem, EstablishmentRating, OrderNote

--- product_browser/models.py
+++ product_browser/models.py
@@ -1,4 +1,37 @@
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
+from site_settings.models import Establishment
 
-# Create your models here.
+class Order(BaseModel):
+    #user = db.ReferenceProperty(User, required=True)
+    ordered_at = db.DateTimeProperty(required=True)
+    status = db.StringProperty(required=True, choices=["PENDING", "ACCEPTED", "COMPLETED", "SERVED"])
+    completed_at = db.DateTimeProperty()
+
+class MenuCategory(BaseModel):
+    title = db.StringProperty(required=True)
+    establishment = db.ReferenceProperty(Establishment, required=True)
+
+class MenuItem(BaseModel):
+    title = db.StringProperty(required=True)
+    price = db.FloatProperty(required=True)
+    currency = db.StringProperty(required=True, choices=["MXN", "USD", "RMB"])
+    menu_category = db.ReferenceProperty(MenuCategory)
+
+class OrderItem(BaseModel):
+    menu_item = db.ReferenceProperty(MenuItem, required=True)
+    order = db.ReferenceProperty(Order, required=True)
+    price = db.FloatProperty(required=True)
+    notes = db.StringProperty()
+
+class EstablishmentRating(BaseModel):
+    establishment = db.ReferenceProperty(Establishment, required=True)
+    rating = db.RatingProperty(required=True)
+    #user = db.ReferenceProperty(User, required=True)
+    comment = db.StringProperty()
+
+class OrderNote(BaseModel):
+    order = db.ReferenceProperty(Order, required=True)
+    note = db.StringProperty(required=True)
+    #user = db.ReferenceProperty(User, required=True)
+
