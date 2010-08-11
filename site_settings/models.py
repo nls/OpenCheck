0dfde56c84e3a95c5eb871a4472998e382023356
Carlos López <carlos@blewblew.com>
2010-08-10 20:54:49 -0500

    Se agregaron modelos de Country, State, City, Address, EstablishmentCategory, Establishment

--- site_settings/models.py
+++ site_settings/models.py
@@ -1,4 +1,32 @@
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
-# Create your models here.
+class Country(BaseModel):
+    name = db.StringProperty(required=True)
+
+class State(BaseModel):
+    name = db.StringProperty(required=True)
+    country = db.ReferenceProperty(Country, required=True)
+
+class City(BaseModel):
+    name = db.StringProperty(required=True)
+    state = db.ReferenceProperty(State, required=True)
+
+class Address(BaseModel):
+    line_1 = db.StringProperty(required=True)
+    line_2 = db.StringProperty()
+    line_3 = db.StringProperty()
+    postal_code = db.StringProperty()
+    coordinates = db.GeoPtProperty()
+    city = db.ReferenceProperty(City, required=True)
+
+class EstablishmentCategory(BaseModel):
+    title = db.StringProperty(required=True)
+    description = db.StringProperty()
+
+class Establishment(BaseModel):
+    name = db.StringProperty(required=True)
+    address = db.ReferenceProperty(Address, required=True)
+    phone = db.StringProperty()
+    description = db.StringProperty()
+    establishment_category = db.ReferenceProperty(EstablishmentCategory)
